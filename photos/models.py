154bd655ae789b288975e6871a77f80b36c51a20
jobmusembi <jobmusembim@gmail.com>
2019-12-15 23:10:48 +0300

    Creating the Location and Category Model

--- photos/models.py
+++ photos/models.py
@@ -4,10 +4,31 @@ from django.db import models
 class Image(models.Model):
 
     img = models.ImageField(upload_to = 'images/', default='path/to/my/default/image.jpg')
-    img name = models.CharField(max_length =30)
-    img description = models.TextField()
+    img_name = models.CharField(max_length =30)
+    img_description = models.TextField()
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.img
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update()
+
+class category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class location(models.Model):
+    location_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.location_name
